cd28b67328848980c73c06ea7bdde0aa54bbf346
JanKozub <j.kozub136@gmail.com>
2020-03-24 15:28:44 +0100

    new server update

--- ThermometerMQTT.ino
+++ ThermometerMQTT.ino
@@ -19,7 +19,7 @@ int lastTime2 = 0;
 boolean state = true;
 
 DHT dht(DHTPIN, DHTTYPE);
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Ustawienie adresu ukladu na 0x27
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Ustawienie adresu ukladu na 0x27
 
 byte tempChar[] = {
   B00100,
